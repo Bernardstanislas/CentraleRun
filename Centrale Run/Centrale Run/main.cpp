344d44492d95bac536d3c873d91ee5c8b27b8a10
Stanislas Bernard <bernardstanislas@gmail.com>
2014-09-29 15:09:55 +0200

    Added an Hello World

---
+++ Centrale Run/Centrale Run/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Centrale Run
+//
+//  Created by Stan on 29/09/2014.
+//  Copyright (c) 2014 Centrale. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
