200289723a5ec93905fe2bfa1e1291fa120afd88
Stanislas Bernard <bernardstanislas@gmail.com>
2014-10-27 13:30:06 +0100

    Update the build script

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 echo "Preparing the build..."
-# echo "Downloading SFML..."
-# wget http://sfml-dev.org/download/sfml/2.1/SFML-2.1-linux-gcc-32bits.tar.bz2
-# echo "Uncompressing SFML..."
-# tar -xvzf SFML-2.1-linux-gcc-32bits.tar.bz2
-
 echo "Installing SFML..."
 sudo apt-get install libsfml-dev
 echo "Building the solution..."
-g++ -std=c++0x Run/*.cpp -lsfml-graphics -lsfml-window -lsfml-system -o out
+cd Run/
+g++ -std=c++0x *.cpp -lsfml-graphics -lsfml-window -lsfml-system -o out
