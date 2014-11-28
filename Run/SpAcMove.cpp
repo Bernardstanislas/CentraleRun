507e1120437db38293dfcda20c1231038de71639
Damien Frikha <damien.frikha@outlook.com>
2014-11-28 01:03:37 +0100

    Add new Direction enum for SpMove

--- Run/SpAcMove.cpp
+++ Run/SpAcMove.cpp
@@ -1,7 +1,8 @@
 #include "SpAcMove.h"
 #include "Sprite.h"
+#include <iostream>
 
-SpAcMove::SpAcMove(int speed, int vspeed, int loopTime) : speed(speed), vspeed(vspeed), loopTime(loopTime), SpriteAction()
+SpAcMove::SpAcMove(unsigned int speed, Direction direction, int loopTime) : speed(speed), direction(direction), loopTime(loopTime), SpriteAction()
 {
 
 }
@@ -12,12 +13,44 @@ FieldAction* SpAcMove::execute()
 
 	if (loopTime != 0 && getTime() % loopTime == 0)
 	{
-		speed = -speed;
-		vspeed = -vspeed;
+		switch (direction)
+		{
+		case Direction::DOWN:
+			direction = Direction::UP;
+			break;
+		case Direction::UP:
+			direction = Direction::DOWN;
+			break;
+		case Direction::LEFT:
+			direction = Direction::RIGHT;
+			break;
+		case Direction::RIGHT:
+			direction = Direction::LEFT;
+			break;
+		}
 	}
 
 	auto XY = source->getPosition();
-	source->setPosition(XY.first + speed, XY.second + vspeed);
+	auto newX = XY.first;
+	auto newY = XY.second;
+
+	switch (direction)
+	{
+	case Direction::DOWN:
+		newY -= speed;
+		break;
+	case Direction::UP:
+		newY += speed;
+		break;
+	case Direction::LEFT:
+		newX -= speed;
+		break;
+	case Direction::RIGHT:
+		newX += speed;
+		break;
+	}
+
+	source->setPosition(newX, newY);
 
 	return nullptr;
 }
--- Run/SpAcMove.h
+++ Run/SpAcMove.h
@@ -1,13 +1,16 @@
 #pragma once
+
 #include "SpriteAction.h"
+#include "Direction.h"
+
 class SpAcMove :
 	public SpriteAction
 {
 protected:
-	int speed;
-	int vspeed;
+	unsigned int speed;
+	Direction direction;
 	int loopTime;
 public:
-	SpAcMove(int speed, int vspeed = 0, int loopTime = 0);
+	SpAcMove(unsigned int speed, Direction direction, int loopTime = 0);
 	FieldAction* execute();
 };
